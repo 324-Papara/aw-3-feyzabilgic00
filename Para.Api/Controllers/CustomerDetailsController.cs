587e786f3d9db0335abd69b3cae1fb3efa918fe2
feyzabilgi <150264038+feyzabilgi@users.noreply.github.com>
2024-07-24 23:47:25 +0300

    .

---
+++ Para.Api/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Para.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerDetailsController : ControllerBase
+    {
+    }
+}
